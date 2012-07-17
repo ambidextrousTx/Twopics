2adfee2a55d4c5ee28dcc6ed3ee08eaf4f555bdb
Ambidextrous <ravisinha@my.unt.edu>
2012-07-17 12:10:23 -0500

    minor spelling, naming changes

--- GetData/automate_tweet_collection.py
+++ GetData/automate_tweet_collection.py
@@ -8,8 +8,8 @@ import commands
 
 # dynamic range; lazily compute on the fly
 for i in xrange(5, 20):
-    commands.getstatusoutput('python get1000tweets.py')
-    commands.getstatusoutput('python cleanTweets.py 1.json 2.json 3.json 4.json 5.json')
+    commands.getstatusoutput('python get_1000_tweets.py')
+    commands.getstatusoutput('python clean_tweets.py 1.json 2.json 3.json 4.json 5.json')
     commands.getstatusoutput('mv allTweets.txt collected_tweets/allTweets_{0}.txt'.format(i))
     time.sleep(60)
     print 'Dealt with %d' % i
