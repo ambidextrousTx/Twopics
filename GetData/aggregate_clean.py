70532a62ed3bf55ca2be265a4f79cbf43e880db1
Ambidextrous <ravisinha@my.unt.edu>
2012-07-17 11:53:47 -0500

    new plan for getting keywords

---
+++ GetData/aggregate_clean.py
@@ -0,0 +1,11 @@
+"""
+Aggregate all collected tweets 
+Remove stop words (from a separate file)
+    - part of speech tagging will only take desired categories
+    - but helps to remove some 
+    - also helps to remove RT, MT, @, #, and hyperlinks
+"""
+
+
+
+
